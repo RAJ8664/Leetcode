f461b4b8f1146276f8c7865296246fc01d921ad8
UNKNOWN <rajr86642@gmail.com>
2024-09-22 15:16:08 +0530

    Time: 340 ms (100%), Space: 56.8 MB (100%) - LeetHub

---
+++ 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long validSubstringCount(String s1, String s2) {
+        long ans = 0;
+        int[] first = new int[26];
+        for(int i = 0; i < s2.length(); i++) first[s2.charAt(i)-'a']++;
+        int n = s1.length();
+        int[] second = new int[26];
+        for(int i = 0, j = 0; i < n; i++) {
+            while(j < n && !check(first, second)) {
+                second[s1.charAt(j)-'a']++;
+                j++;
+            }
+            if(check(first, second))
+                ans += n - j + 1;
+            second[s1.charAt(i) - 'a']--;
+        }
+        return ans;
+    }
+
+
+    private boolean check(int[] a, int[] b) {
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] > b[i]) return false;
+        }
+        return true;
+    }
+}
