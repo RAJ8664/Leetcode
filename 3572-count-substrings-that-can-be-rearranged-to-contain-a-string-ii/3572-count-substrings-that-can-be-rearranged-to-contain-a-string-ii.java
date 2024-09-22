38b9f589a15d1381e43584b5d5a75cc2f841ed4e
UNKNOWN <rajr86642@gmail.com>
2024-09-22 15:16:19 +0530

    Time: 339 ms (100%), Space: 57 MB (100%) - LeetHub

--- 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.java
+++ 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.java
@@ -16,8 +16,7 @@ class Solution {
         }
         return ans;
     }
-
-
+    
     private boolean check(int[] a, int[] b) {
         for(int i = 0; i < a.length; i++) {
             if(a[i] > b[i]) return false;
