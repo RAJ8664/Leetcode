29d0663f47b5be57a51566e454d886a2113afebd
UNKNOWN <rajr86642@gmail.com>
2024-10-02 14:39:10 +0530

    Time: 25 ms (80.58%), Space: 62.3 MB (45.54%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -1,26 +1,24 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n=arr.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[] ans=new int[n];
-        for(int i=0;i<n;i++){
-            ans[i]=arr[i];
-        }
+        int n = arr.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] ans = new int[n];
+        for(int i = 0;i < n; i++) ans[i] = arr[i];
         Arrays.sort(ans);
-        int y=1;
-        for(int i=0;i<n;i++){
+        int y = 1;
+        for(int i = 0;i < n; i++){
             if(!map.containsKey(ans[i])){
-                map.put(ans[i],y);
+                map.put(ans[i], y);
                 y++;
             }
         }
-        int[] raj=new int[n];
-        int k=0;
-        for(int i=0;i<n;i++){
-            int x=map.get(arr[i]);
-            raj[k]=x;
+        int[] answer = new int[n];
+        int k = 0;
+        for(int i = 0;i < n; i++){
+            int x = map.get(arr[i]);
+            answer[k] = x;
             k++;
         }
-        return raj;
+        return answer;
     }
 }
