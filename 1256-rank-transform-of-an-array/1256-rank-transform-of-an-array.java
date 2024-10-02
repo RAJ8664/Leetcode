f29377db5d7e9176647d4cb4f438c6a8af3f55c7
UNKNOWN <rajr86642@gmail.com>
2024-10-02 14:37:23 +0530

    Time: 27 ms (61.24%), Space: 61.2 MB (76.03%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] ans=new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=arr[i];
+        }
+        Arrays.sort(ans);
+        int y=1;
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(ans[i])){
+                map.put(ans[i],y);
+                y++;
+            }
+        }
+        int[] raj=new int[n];
+        int k=0;
+        for(int i=0;i<n;i++){
+            int x=map.get(arr[i]);
+            raj[k]=x;
+            k++;
+        }
+        return raj;
+    }
+}
