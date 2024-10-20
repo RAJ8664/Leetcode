5b27ca03e93fdf62b591943be8535c13e332b87b
UNKNOWN <rajr86642@gmail.com>
2024-10-20 15:50:38 +0530

    Time: 266 ms (100%), Space: 58.4 MB (100%) - LeetHub

---
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] <= nums[i + 1]) continue;
+            int temp = 0;
+            int find = solve(nums[i]);
+            if (find <= 1) return -1;
+            temp++;
+            nums[i] = find;
+            if (nums[i] > nums[i + 1]) return -1;
+            res += temp;
+        }
+        return res;
+    }
+
+    private int solve(int current) {
+        if (current <= 1) return 0;
+        for (int i = 2; i * i <= current; i++) {
+            if (current % i == 0) {
+                if (i < current) return i;
+                if (current / i < current) return current / i;
+            }
+        }
+        return 0;
+    }
+}
