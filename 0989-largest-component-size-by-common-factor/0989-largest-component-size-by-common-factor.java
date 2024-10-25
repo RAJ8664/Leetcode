c30e1419af97c6c4d423c7acbc2d397dfc920e05
UNKNOWN <rajr86642@gmail.com>
2024-10-25 21:01:23 +0530

    Time: 198 ms (16.07%), Space: 59.2 MB (5.36%) - LeetHub

--- 0989-largest-component-size-by-common-factor/0989-largest-component-size-by-common-factor.java
+++ 0989-largest-component-size-by-common-factor/0989-largest-component-size-by-common-factor.java
@@ -1,46 +1,57 @@
 class Solution {
-    private ArrayList<HashSet<Integer>> adj;
     private HashMap<Integer, ArrayList<Integer>> map;
+    static class DSU {
+        private int parent[];
+        private int size[];
+        public DSU(int n) {
+            parent = new int[n + 1];
+            size = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public int find_parent(int u) {
+            if (parent[u] == u) return parent[u] = u;
+            return parent[u] = find_parent(parent[u]);
+        }
+
+        public void unite(int u, int v) {
+            u = find_parent(u);
+            v = find_parent(v);
+            if (u == v) return;
+            if (size[v] > size[u]) {
+                int temp = u;
+                u = v;
+                v = temp;
+            }
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }
+
     public int largestComponentSize(int[] nums) {
         int n = nums.length;
-        adj = new ArrayList<>();
         map = new HashMap<>();
-        for (int i = 0; i <= (int)(1e5 + 1); i++) adj.add(new HashSet<>());
-        HashSet<Integer> set = new HashSet<>();
-        for (int ele : nums) set.add(ele);
-        for (int i = 0; i < n; i++) {
-            int current = nums[i];
-            compute_div(current);
-        }
+        int maxi_ele = 0; for (int ele : nums) maxi_ele = Math.max(maxi_ele, ele);
+        DSU dsu = new DSU(maxi_ele + 1);
+        for (int i = 0; i < n; i++) compute_div(nums[i]);
         for (Map.Entry<Integer, ArrayList<Integer>> curr : map.entrySet()) {
             ArrayList<Integer> res = curr.getValue();
             for (int i = 0; i < res.size() - 1; i++) {
                 int u = res.get(i);
                 int v = res.get(i + 1);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
+                dsu.unite(u , v);
             }
         }
-        int vis[] = new int[(int)(1e5 + 1)];
         int maxi = 0;
         for (int i = 0; i < n; i++) {
-            if (vis[nums[i]] == 0) {
-                ArrayList<Integer> res = new ArrayList<>();
-                dfs(nums[i], vis, res);
-                maxi = Math.max(maxi, res.size());
-            }
+            int current = nums[i];
+            if (dsu.find_parent(current) == current) maxi = Math.max(maxi, dsu.size[current]);
         }
         return maxi;
     }
-
-    private void dfs(int u , int vis[] , ArrayList<Integer> res) {
-        res.add(u);
-        vis[u] = 1;
-        for (int v : adj.get(u)) {
-            if (vis[v] == 0) dfs(v, vis, res);
-        }
-    }
-
     private void compute_div(int n) {
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
