5351b5dd4a4485fa23f9ac14fcc7a224bba4c3fa
UNKNOWN <rajr86642@gmail.com>
2024-11-08 12:36:50 +0000

    Time: 0 ms (100%), Space: 40.7 MB (19.95%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,20 @@
+class Solution {
+    private HashMap<Long, Long> memo;
+    public int integerReplacement(int n) {
+        memo = new HashMap<>();
+        long res = solve((long)(n));
+        return (int)(res);
+    }
+
+    private long solve(long n) {
+        if (n == 1) return 0;
+        if (memo.containsKey((long)(n))) return memo.get(n);
+        if (n % 2 == 0) {
+            return 1 + solve(n / 2);
+        }
+        long op1 = 1 + solve(n + 1);
+        long op2 = 1 + solve(n - 1);
+        memo.put(n , (long)(Math.min(op1, op2)));
+        return (long)(Math.min(op1, op2));
+    }
+}
