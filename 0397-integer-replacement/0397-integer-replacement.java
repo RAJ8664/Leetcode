ebe2ce7628e99a1e8e425ddba6eb85a852d423d4
UNKNOWN <rajr86642@gmail.com>
2024-11-08 12:37:04 +0000

    Time: 1 ms (50.76%), Space: 40.6 MB (28.52%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -9,9 +9,7 @@ class Solution {
     private long solve(long n) {
         if (n == 1) return 0;
         if (memo.containsKey((long)(n))) return memo.get(n);
-        if (n % 2 == 0) {
-            return 1 + solve(n / 2);
-        }
+        if (n % 2 == 0) return 1 + solve(n / 2);
         long op1 = 1 + solve(n + 1);
         long op2 = 1 + solve(n - 1);
         memo.put(n , (long)(Math.min(op1, op2)));
