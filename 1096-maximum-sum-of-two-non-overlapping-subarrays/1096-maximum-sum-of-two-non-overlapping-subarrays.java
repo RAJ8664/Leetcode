d309d6b87e8bf8f9d74f2ea4dba0132b69094023
UNKNOWN <rajr86642@gmail.com>
2024-11-13 19:26:39 +0530

    Time: 3 ms (54.87%), Space: 42 MB (78.93%) - LeetHub

--- 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.java
+++ 1096-maximum-sum-of-two-non-overlapping-subarrays/1096-maximum-sum-of-two-non-overlapping-subarrays.java
@@ -3,17 +3,17 @@ class Solution {
         int n = nums.length;
         int current_maxi = 0, current_sum = 0, start = 0;
         for (int i = 0; i < firstLen; i++) current_sum += nums[i];
-        if (firstLen + secondLen < n) current_maxi = Math.max(current_maxi, current_sum + kadane(nums, firstLen + 1, n - 1, secondLen));
+        if (firstLen + secondLen < n) current_maxi = Math.max(current_maxi, current_sum + solve(nums, firstLen + 1, n - 1, secondLen));
         for (int i = firstLen; i < n; i++) {
             current_sum += nums[i];
             current_sum -= nums[start++];
-            if (start >= secondLen) current_maxi = Math.max(current_maxi, current_sum + kadane(nums, 0, start - 1, secondLen));
-            if (i + 1 + secondLen < n) current_maxi = Math.max(current_maxi, current_sum + kadane(nums, i + 1, n - 1, secondLen));
+            if (start >= secondLen) current_maxi = Math.max(current_maxi, current_sum + solve(nums, 0, start - 1, secondLen));
+            if (i + 1 + secondLen < n) current_maxi = Math.max(current_maxi, current_sum + solve(nums, i + 1, n - 1, secondLen));
         }
         return current_maxi;
     }
 
-    private int kadane(int arr[] , int low, int high, int len) {
+    private int solve(int arr[] , int low, int high, int len) {
         int maxi_sum = 0, current_sum = 0;
         for (int i = low; i < low + len; i++) current_sum += arr[i];
         maxi_sum = Math.max(maxi_sum , current_sum);
