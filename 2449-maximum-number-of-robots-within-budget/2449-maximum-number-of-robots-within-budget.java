91813b9cff02fdfd3d7b9c7d317c5b6a5c09aa24
UNKNOWN <rajr86642@gmail.com>
2024-11-14 23:35:57 +0530

    Time: 1180 ms (6.02%), Space: 57 MB (5.56%) - LeetHub

---
+++ 2449-maximum-number-of-robots-within-budget/2449-maximum-number-of-robots-within-budget.java
@@ -0,0 +1,56 @@
+class Solution {
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
+        int n = chargeTimes.length;
+        int low = 1;
+        int high = n;
+        int ans = 0;
+        long pref[] = new long[n];
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += runningCosts[i];
+            pref[i] = sum;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (ok(mid, chargeTimes, runningCosts, budget, pref)) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+
+    private boolean ok(int mid, int chargeTimes[], int runningCosts[], long budget, long pref[]) {
+        int n = chargeTimes.length;
+        TreeSet<Integer> set = new TreeSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        long current_sum = 0;
+        for (int i = 0; i < mid; i++) {
+            int current = chargeTimes[i];
+            set.add(current);
+            map.put(current, map.getOrDefault(current, 0) + 1);
+            current_sum += runningCosts[i];
+        }
+        int current_maxi = set.last();
+        long current_budget = current_maxi * 1L +  mid * 1L * current_sum;
+        if (current_budget <= budget) return true;
+        int start = 0;
+        for (int i = mid; i < n; i++) {
+            int current = chargeTimes[i];
+            current_sum += runningCosts[i];
+            current_sum -= runningCosts[start];
+            map.put(current, map.getOrDefault(current, 0) + 1);
+            map.put(chargeTimes[start], map.getOrDefault(chargeTimes[start], 0) -1);
+            set.add(current);
+            if (map.getOrDefault(chargeTimes[start], 0) == 0) {
+                map.remove(chargeTimes[start]);
+                set.remove(chargeTimes[start]);
+            }
+            start++;
+            current_budget = set.last() * 1L + mid * (current_sum);
+            if (current_budget <= budget) return true;
+        }
+        return false;
+    }
+}
