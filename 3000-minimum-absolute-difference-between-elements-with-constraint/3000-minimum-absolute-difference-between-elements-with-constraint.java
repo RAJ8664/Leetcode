13017db618a27af5c0502b937da18768d9aa73f7
UNKNOWN <rajr86642@gmail.com>
2024-11-17 02:16:40 +0530

    Time: 123 ms (29.49%), Space: 71.7 MB (5.43%) - LeetHub

--- 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.java
+++ 3000-minimum-absolute-difference-between-elements-with-constraint/3000-minimum-absolute-difference-between-elements-with-constraint.java
@@ -10,26 +10,20 @@ class Solution {
             return "(" + node + " " + ind + ")";
         }
     }
-    static class custom_sort implements Comparator<Pair> {
-        @Override
-        public int compare(Pair first, Pair second) {
-            return Integer.compare(second.ind, first.ind);
-        }
-    }
     public int minAbsoluteDifference(List<Integer> nums, int x) {
         int n = nums.size();
         int mini = Integer.MAX_VALUE;
         if (x == 0) return 0;
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new custom_sort());
+        Queue<Pair> q = new LinkedList<>();
         TreeSet<Integer> set = new TreeSet<>();
         for (int i = n - 1; i >= 0; i--) {
             int current = nums.get(i);
-            if (pq.size() > 0 && Math.abs(pq.peek().ind - i) >= x) set.add(pq.poll().node);
+            if (q.size() > 0 && Math.abs(q.peek().ind - i) >= x) set.add(q.poll().node);
             Integer ceil = set.ceiling(current);
             Integer floor = set.floor(current);
             if (ceil != null) mini = Math.min(mini, Math.abs(current - ceil));
             if (floor != null) mini = Math.min(mini, Math.abs(current - floor));
-            pq.offer(new Pair(current, i));
+            q.offer(new Pair(current, i));
         }
         return mini;
     }
