fcf56239ff6487a6b2407fc3aad650c4be092917
UNKNOWN <rajr86642@gmail.com>
2024-11-17 13:19:52 +0530

    Time: 3 ms (100%), Space: 95.9 MB (100%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -8,11 +8,9 @@ class Solution {
             arr[l]++; 
             if (r + 1 < n) arr[r + 1]--;
         }
-        int pref[] = new int[n];
-        pref[0] = arr[0];
-        for (int i = 1; i < n; i++) pref[i] = pref[i - 1] + arr[i];
+        for (int i = 1; i < n; i++) arr[i] = arr[i - 1] + arr[i];
         for (int i = 0; i < n; i++) {
-            if (pref[i] < nums[i]) return false;
+            if (arr[i] < nums[i]) return false;
         }
         return true;
     }
