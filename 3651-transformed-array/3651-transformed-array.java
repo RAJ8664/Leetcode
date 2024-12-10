d40bdaadd820aa843a4991bd32b4434a75b0360f
UNKNOWN <rajr86642@gmail.com>
2024-12-10 13:06:21 +0530

    Time: 1 ms (99.87%), Space: 44.5 MB (99.85%) - LeetHub

--- 3651-transformed-array/3651-transformed-array.java
+++ 3651-transformed-array/3651-transformed-array.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> res = new ArrayList<>();
+        int ans[] = new int[n];
         for (int i = 0; i < n; i++) {
             int ele = nums[i];
             if (ele >= 0) {
-                if (i + ele < n) res.add(nums[i + ele]);
-                else res.add(nums[(i + ele) % n]);
+                if (i + ele < n) ans[i] = nums[i + ele];
+                else ans[i] = nums[(i + ele) % n];
             }
             else {
-                if (i - Math.abs(ele) >= 0) res.add(nums[i - Math.abs(ele)]);
-                else res.add(nums[(i - Math.abs(ele) % n + n) % n]);
+                if (i - Math.abs(ele) >= 0) ans[i] = nums[i - Math.abs(ele)];
+                else ans[i] = nums[(i - Math.abs(ele) % n + n) % n];
             }
         }
-        int ans[] = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) ans[i] = res.get(i);
         return ans;
     }
 }
