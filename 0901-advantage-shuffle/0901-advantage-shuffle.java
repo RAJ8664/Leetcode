788bc68142bb1f635e11ca714f819318162802ad
UNKNOWN <rajr86642@gmail.com>
2024-12-13 13:40:07 +0530

    Time: 158 ms (17.22%), Space: 68.2 MB (5.26%) - LeetHub

--- 0901-advantage-shuffle/0901-advantage-shuffle.java
+++ 0901-advantage-shuffle/0901-advantage-shuffle.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
-        ArrayList<Integer> res = new ArrayList<>();
+        int ans[] = new int[n];
         TreeSet<Integer> set = new TreeSet<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int ele : nums1) {
@@ -12,19 +12,16 @@ class Solution {
             int current_ele = nums2[i];
             Integer next = set.higher(current_ele);
             if (next != null) {
-                res.add(next);
+                ans[i] = next;
                 map.put(next, map.getOrDefault(next, 0) -1);
                 if (map.getOrDefault(next, 0) == 0) set.remove(next);
             }
             else {
-                res.add(set.first());
+                ans[i] = set.first();
                 map.put(set.first(), map.getOrDefault(set.first(), 0) -1);
                 if (map.getOrDefault(set.first(), 0) == 0) set.remove(set.first());
             }
         }
-        System.out.println(res);
-        int ans[] = new int[res.size()];
-        for (int i = 0; i < res.size(); i++) ans[i] = res.get(i);
         return ans;
     }
 }
