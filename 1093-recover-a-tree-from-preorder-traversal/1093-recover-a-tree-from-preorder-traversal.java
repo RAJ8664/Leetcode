e56fa1353a275d9d69ad1b1eadc0a6fe1bb740fd
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:31:45 +0530

    Update 1093-recover-a-tree-from-preorder-traversal.java

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -9,10 +9,7 @@ class Solution {
                 ++curLevel;
                 ++i;
             }
-            while(i < n && S.charAt(i) >= '0' && S.charAt(i) <= '9') {
-                num = num * 10 + (S.charAt(i) - '0');
-                i++;
-            }
+            while(i < n && S.charAt(i) >= '0' && S.charAt(i) <= '9') num = num * 10 + (S.charAt(i++) - '0');
             TreeNode current = new TreeNode(num);
             map.put(curLevel, current);
             if(curLevel > 0) {
