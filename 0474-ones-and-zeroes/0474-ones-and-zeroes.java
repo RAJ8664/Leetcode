b1f363b352d09d5484f8605eb4cfe65f6b3e8a66
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:06:04 +0530

    Update 0474-ones-and-zeroes.java

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -6,8 +6,7 @@ class Solution {
         int res = solve(0, strs, 0, 0, dp, zero, one);
         return res;
     }
-
-    static int solve(int ind, String arr[] , int zero , int one, int dp[][][], int max_zero, int max_one) {
+    static int solve(int ind, String arr[] , int zero, int one, int dp[][][], int max_zero, int max_one) {
         if (ind >= arr.length) return 0;
         if (dp[ind][zero][one] != -1) return dp[ind][zero][one];
         int count0 = 0, count1 = 0, op1 = 0, op2 = 0;
@@ -16,9 +15,7 @@ class Solution {
             if (current.charAt(i) == '0') count0++;
             else count1++;
         }
-        if (zero + count0 <= max_zero && one + count1 <= max_one) {
-            op1 = 1 + solve(ind + 1, arr, zero + count0 , one + count1 , dp, max_zero, max_one);
-        }
+        if (zero + count0 <= max_zero && one + count1 <= max_one) op1 = 1 + solve(ind + 1, arr, zero + count0 , one + count1 , dp, max_zero, max_one);
         op2 = solve(ind + 1, arr, zero, one, dp, max_zero, max_one);
         return dp[ind][zero][one] = Math.max(op1, op2);
     }
