699e974935eb9dfcae0ca26568eac626f8160c5c
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:16:32 +0530

    Update 0860-design-circular-queue.java

--- 0860-design-circular-queue/0860-design-circular-queue.java
+++ 0860-design-circular-queue/0860-design-circular-queue.java
@@ -5,38 +5,31 @@ class MyCircularQueue {
         this.len = k;
         dq = new ArrayDeque<>();
     }
-    
     public boolean enQueue(int value) {
         if (dq.size() == len) return false;
         dq.addLast(value);
         return true;
     }
-    
     public boolean deQueue() {
         if (dq.size() == 0) return false;
         dq.pollFirst();
         return true;
     }
-    
     public int Front() {
         if (dq.size() == 0) return -1;
         return dq.peekFirst();
     }
-    
     public int Rear() {
         if (dq.size() == 0) return -1;
         return dq.peekLast();
     }
-    
     public boolean isEmpty() {
         return dq.size() == 0;
     }
-    
     public boolean isFull() {
         return dq.size() == len;
     }
 }
-
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
  * MyCircularQueue obj = new MyCircularQueue(k);
