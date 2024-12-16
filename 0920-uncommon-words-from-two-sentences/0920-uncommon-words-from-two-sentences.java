4919ea8b37042fce62f98eefbb0e501605ca233a
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:24:19 +0530

    Update 0920-uncommon-words-from-two-sentences.java

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.java
@@ -1,7 +1,6 @@
 class Solution {
     public String[] uncommonFromSentences(String s1, String s2) {
-        s1 += " ";
-        s2 += " ";
+        s1 += " "; s2 += " ";
         int n = s1.length(), m = s2.length();
         HashSet<String> first = new HashSet<>();
         HashSet<String> second = new HashSet<>();
@@ -19,7 +18,6 @@ class Solution {
             }
             else current += s1.charAt(i);
         }
-
         current = "";
         for (int i = 0; i < m; i++) {
             if (s2.charAt(i) == ' ') {
@@ -32,7 +30,6 @@ class Solution {
             }
             else current += s2.charAt(i);
         }
-
         ArrayList<String> res = new ArrayList<>();
         for (String temp : first) if (!second.contains(temp)) if (!res.contains(temp) && !firstR.contains(temp) && !secondR.contains(temp)) res.add(temp);
         for (String temp : second) if (!first.contains(temp)) if (!res.contains(temp) && !firstR.contains(temp) && !secondR.contains(temp)) res.add(temp);
