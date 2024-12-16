d65d692f2b0dfeb7e8511e95868f45a7ae8353ec
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:13:26 +0530

    Update 0852-friends-of-appropriate-ages.java

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numFriendRequests(int[] ages) {
-        int n = ages.length;
-        int count = 0;
+        int n = ages.length, count = 0;
         Arrays.sort(ages);
         for (int i = 0; i < n / 2; i++) {
             int temp = ages[i];
@@ -11,20 +10,15 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             int current = ages[i];
-            int res = binarySearch(i , current, ages);
+            int res = binarySearch(i, current, ages);
             if (!map.containsKey(current)) map.put(current, res);
         }
-        for (int i = 0; i < n; i++) {
-            count += map.getOrDefault(ages[i], 0);
-        }
+        for (int i = 0; i < n; i++) count += map.getOrDefault(ages[i], 0);
         return count;
     }
-
     private int binarySearch(int current_ind, int current, int ages[]) {
         int n = ages.length;
-        int low = current_ind + 1;
-        int high = n - 1;
-        int ans = -1;
+        int low = current_ind + 1, high = n - 1, ans = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (ages[mid] <= current && ages[mid] > 0.5 * current + 7 && !(ages[mid] > 100 && current < 100)) {
