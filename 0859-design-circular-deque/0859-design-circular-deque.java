522fca6bacc6994aac3169019de9b1cc3dc1a0c1
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:16:09 +0530

    Update 0859-design-circular-deque.java

--- 0859-design-circular-deque/0859-design-circular-deque.java
+++ 0859-design-circular-deque/0859-design-circular-deque.java
@@ -5,50 +5,41 @@ class MyCircularDeque {
         dq = new ArrayDeque<>();
         this.len = k;
     }
-    
     public boolean insertFront(int value) {
         if (dq.size() == len) return false;
         dq.addFirst(value);
         return true;
     }
-    
     public boolean insertLast(int value) {
         if (dq.size() == len) return false;
         dq.addLast(value);
         return true;
     }
-    
     public boolean deleteFront() {
         if (dq.size() == 0) return false;
         dq.pollFirst();
         return true;
     }
-    
     public boolean deleteLast() {
         if (dq.size() == 0) return false;
         dq.pollLast();
         return true;
     }
-    
     public int getFront() {
         if (dq.size() == 0) return -1;
         return dq.peekFirst();
     }
-    
     public int getRear() {
         if (dq.size() == 0) return -1;
         return dq.peekLast();
     }
-    
     public boolean isEmpty() {
         return dq.size() == 0;
     }
-    
     public boolean isFull() {
         return dq.size() == len;
     }
 }
-
 /**
  * Your MyCircularDeque object will be instantiated and called as such:
  * MyCircularDeque obj = new MyCircularDeque(k);
