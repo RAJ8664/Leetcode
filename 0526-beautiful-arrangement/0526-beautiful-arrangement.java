078d1a8c9842b1fd238242dc39039469e3832019
UNKNOWN <rajr86642@gmail.com>
2024-12-16 11:06:52 +0530

    Update 0526-beautiful-arrangement.java

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -3,11 +3,10 @@ class Solution {
     public int countArrangement(int n) {
         ans = 0;
         int vis[] = new int[n + 1];
-        solve(vis, n , 1);
+        solve(vis, n, 1);
         return ans;
     }
-
-    private void solve(int vis[] , int n , int curr_num) {
+    private void solve(int vis[], int n, int curr_num) {
         if (curr_num == n + 1) {
             ans++;
             return;
@@ -16,11 +15,10 @@ class Solution {
             if (vis[i] == 0) {
                 if (curr_num % i == 0 || i % curr_num == 0) {
                     vis[i] = 1;
-                    solve(vis, n , curr_num + 1);
+                    solve(vis, n, curr_num + 1);
                     vis[i] = 0;
                 }
             }
         }
     }
-
 }
