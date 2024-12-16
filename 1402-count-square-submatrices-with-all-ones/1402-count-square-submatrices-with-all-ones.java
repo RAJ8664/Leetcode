371fdf26e32788ea260bcaf7edc346013c3f3de6
UNKNOWN <rajr86642@gmail.com>
2024-12-16 10:44:43 +0530

    Update 1402-count-square-submatrices-with-all-ones.java

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,18 +1,14 @@
 class Solution {
     static int prefix[][];
     public int countSquares(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        int n = matrix.length, m = matrix[0].length;
         prefix = new int[n + 1][m + 1];
         build(matrix);
         int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 1) {
-                    int x1 = i;
-                    int y1 = j;
-                    int x2 = i;
-                    int y2 = j;
+                    int x1 = i, y1 = j, x2 = i, y2 = j;
                     while (x2 < n && y2 < m) {
                         int req_area = (y2 - y1 + 1) * (x2 - x1 + 1);
                         int query = query(x1 + 1, y1 + 1, x2 + 1, y2 + 1);
@@ -25,11 +21,9 @@ class Solution {
         }
         return count;
     }
-
     static int query(int x1, int y1, int x2, int y2) {
         return prefix[x2][y2] + prefix[x1 - 1][y1 - 1] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1];
     }
-
     static void build(int matrix[][]) {
         int n = matrix.length;
         int m = matrix[0].length;
