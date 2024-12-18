3e3a082854490243ddece8386490b422ac22257b
UNKNOWN <rajr86642@gmail.com>
2024-12-18 11:50:48 +0530

    Time: 4 ms (40.47%), Space: 44.2 MB (17.88%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int next_smallest[] = new int[n];
         int res[] = new int[n];
         Stack<Integer> st = new Stack<>();
         for (int i = n - 1; i >= 0; i--) {
