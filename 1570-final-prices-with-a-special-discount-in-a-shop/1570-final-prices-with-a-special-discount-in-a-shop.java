07008a1559d62169061599acec32dc165fa7f477
UNKNOWN <rajr86642@gmail.com>
2024-12-18 11:50:36 +0530

    Time: 4 ms (40.47%), Space: 44.3 MB (13.19%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,28 +1,16 @@
 class Solution {
-    static class Pair {
-        int node, ind;
-        public Pair(int node, int ind) {
-            this.node = node;
-            this.ind = ind;
-        }
-        @Override
-        public String toString() {
-            return "(" + node + " " + ind + ")";
-        }
-    }
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
         int next_smallest[] = new int[n];
-        Stack<Pair> st = new Stack<>();
+        int res[] = new int[n];
+        Stack<Integer> st = new Stack<>();
         for (int i = n - 1; i >= 0; i--) {
             int current = prices[i];
-            while (st.size() > 0 && st.peek().node > current) st.pop();
-            if (st.size() == 0) next_smallest[i] = 0;
-            else next_smallest[i] = st.peek().node;
-            st.add(new Pair(current, i));
+            while (st.size() > 0 && st.peek() > current) st.pop();
+            if (st.size() == 0) res[i] = current;
+            else res[i] = current - st.peek();
+            st.add(current);
         }
-        int res[] = new int[n];
-        for (int i = 0; i < n; i++) res[i] = prices[i] - next_smallest[i];
         return res;
     }
 }
