4b8646dad31bbc893ac788c967ed77877c1b87a8
UNKNOWN <rajr86642@gmail.com>
2024-12-27 10:54:46 +0530

    Time: 3 ms (97.03%), Space: 54.8 MB (5.14%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -4,15 +4,15 @@ class Solution {
         int arr[] = new int[n];
         for (int i = 0; i < n; i++) arr[i] = values[i] - i;
         int maxi = Integer.MIN_VALUE;
-        int pref[] = new int[n];
+        int suff_maxi[] = new int[n];
         for (int i = n - 1; i >= 0; i--) {
             maxi = Math.max(maxi, arr[i]);
-            pref[i] = maxi;
+            suff_maxi[i] = maxi;
         }
         int res = 0;
         for (int i = 0; i < n - 1; i++) {
             int current_sum = values[i] + i;
-            current_sum += (pref[i + 1]);
+            current_sum += (suff_maxi[i + 1]);
             res = Math.max(res, current_sum);
         }
         return res;
