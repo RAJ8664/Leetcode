e553db75925298957315c01ceea5f5e424c29bc6
UNKNOWN <rajr86642@gmail.com>
2025-01-05 10:43:36 +0530

    Time: 40 ms (100%), Space: 45.3 MB (100%) - LeetHub

--- 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.java
+++ 3634-find-mirror-score-of-a-string/3634-find-mirror-score-of-a-string.java
@@ -3,13 +3,14 @@ class Solution {
         int n = s.length();
         long ans = 0;
         Map<Integer, List<Integer>> mp = new HashMap<>();
-        for(int i= 0; i < s.length(); ++i){
+        for(int i = 0; i < s.length(); ++i){
             int cur = s.charAt(i) - 'a';
             int mirror = 25 - cur;
             if(mp.containsKey(mirror) && mp.get(mirror).size() > 0){
                 ans += (long)i - (long)mp.get(mirror).get(mp.get(mirror).size() - 1);
                 mp.get(mirror).remove(mp.get(mirror).size() - 1);
-            }else mp.computeIfAbsent(cur, key -> new ArrayList<>()).add(i);
+            }
+            else mp.computeIfAbsent(cur, key -> new ArrayList<>()).add(i);
         }
         return ans;
     }
