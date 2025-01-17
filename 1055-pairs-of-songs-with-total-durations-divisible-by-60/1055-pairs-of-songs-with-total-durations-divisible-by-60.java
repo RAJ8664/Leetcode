be49c8ae6f572f418342d0148a149e3095fbd24a
UNKNOWN <rajr86642@gmail.com>
2025-01-17 23:41:39 +0530

    Time: 5 ms (46.02%), Space: 53.5 MB (20.56%) - LeetHub

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/1055-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,11 +1,11 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
         int n = time.length;
-        int freq[] = new int[2000];
+        int freq[] = new int[1001];
         int count = 0;
         for (int i = 0; i < n; i++) {
             int current = time[i];
-            for (int j = 60; j <= 2000; j += 60) if (j - current > 0) count += freq[j - current];
+            for (int j = 60; j <= 1000; j += 60) if (j - current > 0) count += freq[j - current];
             freq[current]++;
         }
         return count;
