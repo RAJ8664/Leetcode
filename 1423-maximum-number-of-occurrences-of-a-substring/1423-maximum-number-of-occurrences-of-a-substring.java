9d797a7919e4fedd6d2aba753b5f496b5fd7ce6e
UNKNOWN <rajr86642@gmail.com>
2025-01-28 22:59:05 +0530

    Time: 1320 ms (5.03%), Space: 146.5 MB (9.87%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.java
@@ -1,33 +1,24 @@
 class Solution {
+    private ArrayList<String> ans;
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        ArrayList<String> res = new ArrayList<>();
-        for (int i = minSize; i <= maxSize; i++) {
-            ArrayList<String> temp = new ArrayList<>();
-            temp = getSubstringsOfLengthK(s, i);
-            for (String x : temp) res.add(x);
-        }
-        ArrayList<String> ans = new ArrayList<>();
-        for (String x : res) {
-            HashSet<Character> set = new HashSet<>();
-            for (int i = 0; i < x.length(); i++) set.add(x.charAt(i));
-            if (set.size() <= maxLetters) ans.add(x);
-        }
+        int n = s.length();
+        ans = new ArrayList<>();
+        for (int i = minSize; i <= maxSize; i++) getSubstringsOfLengthK(s, i, maxLetters);
         HashMap<String, Integer> map = new HashMap<>();
         for (String x : ans) map.put(x, map.getOrDefault(x, 0) + 1);
         int maxi = 0;
-        for (Map.Entry<String, Integer> curr : map.entrySet()) {
-            String key = curr.getKey();
-            int val = curr.getValue();
-            maxi = Math.max(maxi, val);
-        }
+        for (int current : map.values()) maxi = Math.max(maxi, current);
         return maxi;
     }
-    private ArrayList<String> getSubstringsOfLengthK(String s, int k) {
+    private ArrayList<String> getSubstringsOfLengthK(String s, int k, int maxLetters) {
         int n = s.length();
         ArrayList<String> res = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (i + k <= n) {
-                res.add(s.substring(i, i + k));
+                String current = s.substring(i, i + k);
+                HashSet<Character> set = new HashSet<>();
+                for (int j = 0; j < current.length(); j++) set.add(current.charAt(j));
+                if (set.size() <= maxLetters) ans.add(current);
             }
         }
         return res;
