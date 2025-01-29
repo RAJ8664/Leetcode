a8f1c98b32e719e9a606433a929f8c24420fcf48
UNKNOWN <rajr86642@gmail.com>
2025-01-29 12:53:43 +0530

    Time: 7 ms (15.25%), Space: 44.9 MB (20%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,33 +1,26 @@
 class Solution {
+    private ArrayList<ArrayList<Integer>> adj;
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         int ans[] = new int[2];
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i <= n + 1; i++) {
-            adj.add(new ArrayList<>());
-        }
+        adj = new ArrayList<>();
+        for(int i = 0; i <= n + 1; i++) adj.add(new ArrayList<>());
         int vis[] = new int[n + 1];
         for(int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
             Arrays.fill(vis,0);
-            if(cycle(u,-1,adj,vis) == true) {
-                ans[0] = u;
-                ans[1] = v;
-                return ans;
-            }
+            if(cycle(edges[i][0], -1, vis) == true) return new int[]{edges[i][0], edges[i][1]};
          }
-         return ans;
+         return new int[]{0, 0};
     }
-    public static boolean cycle(int u,int par,  ArrayList<ArrayList<Integer>> adj, int vis[]) {
+    private boolean cycle(int u, int par, int vis[]) {
         vis[u] = 1;
         boolean temp = false;
-        for(int child : adj.get(u)) {
-            if(vis[child] == 1 && child == par) continue;
-            if(vis[child] == 1) return true;
-            temp |= cycle(child, u, adj,vis);
+        for (int child : adj.get(u)) {
+            if (vis[child] == 1 && child == par) continue;
+            if (vis[child] == 1) return true;
+            temp |= cycle(child, u, vis);
         }
         return temp;
     }
