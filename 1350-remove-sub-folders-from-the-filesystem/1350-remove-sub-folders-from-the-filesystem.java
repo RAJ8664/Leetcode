7b6eefe3bb480d06f6bb1dc9fc67af31b89a9208
Rk Roy <rajr86642@gmail.com>
2025-07-19 12:56:53 +0545

    Time: 770 ms (5.08%), Space: 54.3 MB (96.48%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -11,26 +11,30 @@ class Solution {
         Arrays.sort(folder, new custom_sort());
         int vis[] = new int[n];
         for (int i = 0; i < n; i++) {
-            if (vis[i] == 1) continue;
+            if (vis[i] == 1) 
+                continue;
             res.add(folder[i]);
             vis[i] = 1;
             for (int j = i + 1; j < n; j++) {
-                if (check(folder[i] , folder[j])) vis[j] = 1;
+                if (check(folder[i] , folder[j])) 
+                    vis[j] = 1;
             }
         }
         return res;
     }
-
     private boolean check(String first, String second) {
         int n = first.length();
         int m = second.length();
         if (n == m) {
-            if (second.startsWith(first)) return true;
+            if (second.startsWith(first)) 
+                return true;
         }
         for (int i = 0; i < Math.min(n , m); i++) {
-            if (first.charAt(i) != second.charAt(i)) return false;
+            if (first.charAt(i) != second.charAt(i)) 
+                return false;
         }
-        if (second.charAt(n) == '/') return true;
+        if (second.charAt(n) == '/') 
+            return true;
         return false;
     }
 }
