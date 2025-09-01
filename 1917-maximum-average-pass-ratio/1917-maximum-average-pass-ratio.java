ad8cfd6c6472a49d3a1e07f2761aa543c7b5e588
Rk Roy <rajr86642@gmail.com>
2025-09-01 07:22:22 +0530

    Time: 348 ms (62.69%), Space: 95.3 MB (69.43%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -1,37 +1,37 @@
 class Solution {
-    static class Pair {
-        double delta;
-        double pass;
-        double total;
-        public Pair(double delta, double pass, double total) {
+    static class Tuple {
+        double delta, pass, total;
+        public Tuple(double delta, double pass, double total) {
             this.delta = delta;
             this.pass = pass;
             this.total = total;
         }
-    }
-    static class sorting implements Comparator<Pair> {
         @Override
-        public int compare(Pair first, Pair second) {
+        public String toString() {
+            return "(" + delta + " " + pass + " " + total + ")";
+        }
+    }
+    static class sorting implements Comparator<Tuple> {
+        @Override
+        public int compare(Tuple first, Tuple second) {
             return Double.compare(second.delta , first.delta);
         }
     }
     public double maxAverageRatio(int[][] classes, int extraStudents) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new sorting());
+        PriorityQueue<Tuple> pq = new PriorityQueue<>(new sorting());
         for(int current[] : classes) {
             double pass = (double)current[0];
             double total = (double)current[1];
             double delta = (double)(pass + 1) / (double)(total + 1) - (double)(pass) / (double)(total);
-            pq.offer(new Pair(delta, pass, total)); 
+            pq.offer(new Tuple(delta, pass, total)); 
         }
         while(extraStudents > 0) {
-            double pass = pq.peek().pass;
-            double total = pq.peek().total;
-            double delta = pq.peek().delta;
+            double pass = pq.peek().pass, total = pq.peek().total, delta = pq.peek().delta;
             pass++;
             total++;
             double newDelta = (double)(pass + 1) / (double)(total + 1) - (double)(pass) / (double)(total);
             pq.poll();
-            pq.offer(new Pair(newDelta, pass, total));
+            pq.offer(new Tuple(newDelta, pass, total));
             extraStudents--;
         }
         double ans = 0;
@@ -43,3 +43,5 @@ class Solution {
         return (double)(ans / (double) total);
     }
 }
+
+ 
