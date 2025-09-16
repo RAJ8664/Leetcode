1da3f0cb7dc4a318fce9d8aa8d8d1e3571320a14
Rk Roy <rajr86642@gmail.com>
2025-09-16 09:08:57 +0530

    Time: 15 ms (6.62%), Space: 45.8 MB (15.56%) - LeetHub

---
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] maxKDistinct(int[] nums, int k) {
+        int n = nums.length;
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int ele : nums)
+            set.add(ele);
+        int res[] = new int[Math.min(k, set.size())];
+        int idx = 0;
+        System.out.println(set);
+        while (k > 0 && set.size() > 0) {
+            Integer curr = set.last();
+            res[idx++] = curr;
+            set.remove(curr);
+            k--;
+        }
+        return res;
+    }
+}
