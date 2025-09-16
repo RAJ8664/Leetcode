bf3986ed007e72b6866ab109e9cf39829f4021eb
Rk Roy <rajr86642@gmail.com>
2025-09-16 15:39:49 +0530

    Time: 151 ms (22.91%), Space: 75.4 MB (63.41%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -1,7 +1,10 @@
 class Solution {
+    private int vis[][];
+    private int id[][];
+    private int size[];
+    private int currId;
     static class Pair {
-        int row;
-        int col;
+        int row, col;
         public Pair(int row, int col) {
             this.row = row;
             this.col = col;
@@ -10,93 +13,69 @@ class Solution {
         public String toString() {
             return "(" + row + " " + col + ")";
         }
-    }
-    static class DSU {
-        private int parent[];
-        private int size[];
-        public DSU(int n) {
-            parent = new int[n + 1];
-            size = new int[n + 1];
-            for (int i = 0; i <= n; i++) {
-                parent[i] = i;
-                size[i] = 1;
-            }
-        }
-        public int Leader(int u) {
-            if (parent[u] == u) return parent[u];
-            return parent[u] = Leader(parent[u]);
-        }
-        public void unite(int u , int v) {
-            u = Leader(u);
-            v = Leader(v);
-            if (u == v) return;
-            if (size[v] > size[u]) {
-                int temp = u;
-                u = v;
-                v = temp;
-            }
-            parent[v] = u;
-            size[u] += size[v];
-        }
-    }
+    }    
     public int largestIsland(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        DSU dsu = new DSU(n * m + 1);
-        int vis[][] = new int[n + 1][m + 1];
+        int n = grid.length, m = grid[0].length;
+        vis = new int[n][m];
+        id = new int[n][m];
+        size = new int[n * m + 1];
+        currId = 1;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (vis[i][j] == 0 && grid[i][j] == 1) {
-                    BFS(i , j , vis, grid, dsu);
+                    BFS(i, j, grid);
+                    currId++;
                 }
             }
         }
-        int maxi = 0;
-        for (int i = 0; i <= n * m; i++) maxi = Math.max(maxi, dsu.size[dsu.Leader(i)]);
-        int dir[][] = {{-1, 0} , {1, 0} , {0, -1} , {0 , 1}};
+
+        int maxi = 1;
+        for (int x : size) 
+            maxi = Math.max(maxi, x);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 0) {
                     HashSet<Integer> ids = new HashSet<>();
-                    for (int dire[] : dir) {
-                        int nr = i + dire[0];
-                        int nc = j + dire[1];
-                        if (nr < n && nc < m && nr >= 0 && nc >= 0 && grid[nr][nc] == 1) {
-                            int id = nr * m + nc;
-                            ids.add(dsu.Leader(id));
-                        }
+                   
+                    if (i - 1 >= 0) ids.add(id[i - 1][j]);
+                    if (j - 1 >= 0) ids.add(id[i][j - 1]);
+                    if (i + 1 < n) ids.add(id[i + 1][j]);
+                    if (j + 1 < m) ids.add(id[i][j + 1]);
+
+                    int compSum = 1;
+                    for (int curIds : ids) {
+                        compSum += size[curIds]; 
                     }
-                    int sum = 0;
-                    for (int ele : ids) sum += dsu.size[ele];
-                    sum++;
-                    maxi = Math.max(maxi, sum);
+                    maxi = Math.max(maxi, compSum);
                 }
             }
         }
         return maxi;
     }
-    private void BFS(int row, int col, int vis[][], int grid[][], DSU dsu) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int dir[][] = {{-1, 0} , {1, 0} , {0, 1} , {0, -1}};
+
+    private void BFS(int startRow, int startCol, int grid[][]) {
+        int n = grid.length, m = grid[0].length;
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(row, col));
-        vis[row][col] = 1;
+        int compSize = 1;
+    
+        q.offer(new Pair(startRow, startCol));
+        id[startRow][startCol] = currId;
+        vis[startRow][startCol] = 1;
+        int dir[][] = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
         while (q.size() > 0) {
-            int cr = q.peek().row;
-            int cc = q.peek().col;
-            int current_id = cr * m + cc;
+            int currRow = q.peek().row, currCol = q.peek().col;
             q.poll();
             for (int dire[] : dir) {
-                int nr = cr + dire[0];
-                int nc = cc + dire[1];
-                int new_id = nr * m + nc;
-                if (nr < n && nc < m && nr >= 0 && nc >= 0 && vis[nr][nc] == 0 && grid[nr][nc] == 1) {
-                    vis[nr][nc] = 1;
-                    q.offer(new Pair(nr, nc));
-                    dsu.unite(current_id, new_id);
+                int newRow = currRow + dire[0], newCol = currCol + dire[1];
+                if (newRow < n && newCol < m && newRow >= 0 && newCol >= 0 && vis[newRow][newCol] == 0 && grid[newRow][newCol] == 1) {
+                    vis[newRow][newCol] = 1;
+                    q.offer(new Pair(newRow, newCol));
+                    id[newRow][newCol] = currId;
+                    compSize++;
                 }
             }
         }
+        size[currId] = compSize;
     }
 }
