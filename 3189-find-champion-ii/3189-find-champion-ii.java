aa1bdd5f794fe5ae2a6da4e0f444101e837da7f7
Rk Roy <rajr86642@gmail.com>
2025-09-18 20:33:00 +0530

    Time: 1 ms (100%), Space: 45.7 MB (56.63%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.java
+++ 3189-find-champion-ii/3189-find-champion-ii.java
@@ -1,31 +1,18 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i <= n + 1; i++) adj.add(new ArrayList<>());
-        for(int current[] : edges) {
-            int u = current[0];
-            int v = current[1];
-            adj.get(u).add(v);
+        int indegree[] = new int[n];
+        for (int edge[] : edges) {
+            int u = edge[0], v = edge[1];
+            indegree[v]++;
         }
-        int cnt = 0, ans = -1;
-        for(int i = 0; i < n; i++) {
-            int count[] = new int[1];
-            int vis[] = new int[n + 1];
-            dfs(i , adj, count, vis);
-            if(count[0] == n) {
-                cnt++;
-                if(cnt > 1) return -1;
+        int count = 0;
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
                 ans = i;
+                count++;
             }
-            count[0] = 0;
         }
-        return ans;
-    }
-    private void dfs(int u , ArrayList<ArrayList<Integer>> adj , int count[], int vis[]) {
-        count[0]++;
-        vis[u] = 1;
-        for(int v : adj.get(u)) {
-            if(vis[v] == 0) dfs(v, adj, count,vis);
-        }
+        return count == 1 ? ans : -1;
     }
 }
