84e79dfd5372c8299404e478e4a5ee689a140fd3
Rk Roy <rajr86642@gmail.com>
2025-09-21 10:37:16 +0530

    Time: 574 ms (70.42%), Space: 206.8 MB (28.17%) - LeetHub

--- 2023-design-movie-rental-system/2023-design-movie-rental-system.java
+++ 2023-design-movie-rental-system/2023-design-movie-rental-system.java
@@ -1,160 +1,140 @@
 class MovieRentingSystem {
+    
+    // Custom Pair class for shop-price mapping
     static class Pair {
-        int movie, price;
+        int movie, price;  // 'movie' is overloaded to store shopId
+        
         public Pair(int movie, int price) {
             this.movie = movie;
             this.price = price;
         }
-        @Override
-        public String toString() {
-            return "(" + movie + " " + price + ")";
-        }
+        
         @Override
         public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            Pair current = (Pair)(obj);
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Pair current = (Pair) obj;
             return current.movie == movie && current.price == price;
         }
+        
         @Override
         public int hashCode() {
             return Objects.hash(movie, price);
         }
     }
-
+    
+    // Tuple class for complete movie information
     static class Tuple {
         int shop, movie, price;
+        
         public Tuple(int shop, int movie, int price) {
             this.shop = shop;
             this.movie = movie;
             this.price = price;
         }
-        @Override
-        public String toString() {
-            return "(" + shop + " " + movie + " " + price + ")";
-        }
+        
         @Override
         public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            Tuple current = (Tuple)(obj);
-            return current.shop == shop && current.movie == movie && current.price == price;
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Tuple current = (Tuple) obj;
+            return current.shop == shop && 
+                   current.movie == movie && 
+                   current.price == price;
         }
+        
         @Override
         public int hashCode() {
             return Objects.hash(shop, movie, price);
         }
     }
-
-    static class customSort implements Comparator<Pair> {
-        @Override
-        public int compare(Pair first, Pair second) {
-            int op1 = Integer.compare(first.price, second.price);
-            if (op1 != 0)
-                return op1;
-            return Integer.compare(first.movie, second.movie);
-        }
-    }
-
+    
+    // Comparator for sorting Tuples
     static class customSortTuple implements Comparator<Tuple> {
         @Override
         public int compare(Tuple first, Tuple second) {
-            int op1 = Integer.compare(first.price, second.price);
-            if (op1 != 0) return op1;
-            int op2 = Integer.compare(first.shop, second.shop);
-            if (op2 != 0) return op2;
+            int priceCmp = Integer.compare(first.price, second.price);
+            if (priceCmp != 0) return priceCmp;
+            
+            int shopCmp = Integer.compare(first.shop, second.shop);
+            if (shopCmp != 0) return shopCmp;
+            
             return Integer.compare(first.movie, second.movie);
         }
     }
-
+    
+    // Comparator for sorting shops by price
     static class customSortShop implements Comparator<Pair> {
         @Override
         public int compare(Pair first, Pair second) {
-            int op1 = Integer.compare(first.price, second.price);
-            if (op1 != 0)
-                return op1;
-            return Integer.compare(first.movie, second.movie); // movie field stores shopId here
+            int priceCmp = Integer.compare(first.price, second.price);
+            if (priceCmp != 0) return priceCmp;
+            return Integer.compare(first.movie, second.movie); // movie stores shopId
         }
     }
-
-    private HashMap<Integer, TreeSet<Pair>> map;
-    private HashMap<Pair, Integer> shopMoviePrice; // key: Pair(shop, movie) stored as (movie=shop, price=movie)
-    private TreeSet<Tuple> rentedMovies;
-    private TreeSet<Tuple> unRentedMovies; 
+    
+    private HashMap<Pair, Integer> shopMoviePrice;
     private HashMap<Integer, TreeSet<Pair>> unRentedMap;
-
+    private TreeSet<Tuple> rentedMovies;
+    
     public MovieRentingSystem(int n, int[][] entries) {
-        map = new HashMap<>();
         shopMoviePrice = new HashMap<>();
         rentedMovies = new TreeSet<>(new customSortTuple());
-        unRentedMovies = new TreeSet<>(new customSortTuple());
         unRentedMap = new HashMap<>();
-
-        for (int entry[] : entries) {
+        
+        for (int[] entry : entries) {
             int shop = entry[0], movie = entry[1], price = entry[2];
-            if (!map.containsKey(shop))
-                map.put(shop, new TreeSet<>(new customSort())); 
-            map.get(shop).add(new Pair(movie, price));
             shopMoviePrice.put(new Pair(shop, movie), price);
-            unRentedMovies.add(new Tuple(shop, movie, price));
-            if (!unRentedMap.containsKey(movie)) 
+            
+            if (!unRentedMap.containsKey(movie)) {
                 unRentedMap.put(movie, new TreeSet<>(new customSortShop()));
+            }
             unRentedMap.get(movie).add(new Pair(shop, price));
-        }   
+        }
     }
     
     public List<Integer> search(int movie) {
         TreeSet<Pair> current = unRentedMap.get(movie);
         List<Integer> res = new ArrayList<>();
         if (current == null) return res;
+        
         Iterator<Pair> it = current.iterator();
         while (res.size() < 5 && it.hasNext()) {
-            res.add(it.next().movie); // here .movie holds shopId for unRentedMap
-        } 
-        return res; 
+            res.add(it.next().movie); // movie field holds shopId
+        }
+        return res;
     }
     
     public void rent(int shop, int movie) {
         int price = shopMoviePrice.get(new Pair(shop, movie));
         Tuple t = new Tuple(shop, movie, price);
         rentedMovies.add(t);
-        unRentedMovies.remove(t);
+        
         TreeSet<Pair> set = unRentedMap.get(movie);
-        if (set != null) set.remove(new Pair(shop, price));
+        if (set != null) {
+            set.remove(new Pair(shop, price));
+        }
     }
     
     public void drop(int shop, int movie) {
         int price = shopMoviePrice.get(new Pair(shop, movie));
         Tuple t = new Tuple(shop, movie, price);
-        rentedMovies.remove(t); 
-        unRentedMovies.add(t);
-        if (!unRentedMap.containsKey(movie)) 
+        rentedMovies.remove(t);
+        
+        if (!unRentedMap.containsKey(movie)) {
             unRentedMap.put(movie, new TreeSet<>(new customSortShop()));
+        }
         unRentedMap.get(movie).add(new Pair(shop, price));
     }
     
     public List<List<Integer>> report() {
         List<List<Integer>> res = new ArrayList<>();
         Iterator<Tuple> it = rentedMovies.iterator();
+        
         while (res.size() < 5 && it.hasNext()) {
             Tuple current = it.next();
-            List<Integer> tempRes = new ArrayList<>();
-            tempRes.add(current.shop); tempRes.add(current.movie);
-            res.add(new ArrayList<>(tempRes));
-        } 
+            res.add(Arrays.asList(current.shop, current.movie));
+        }
         return res;
     }
 }
-
-/**
- * Your MovieRentingSystem object will be instantiated and called as such:
- * MovieRentingSystem obj = new MovieRentingSystem(n, entries);
- * List<Integer> param_1 = obj.search(movie);
- * obj.rent(shop,movie);
- * obj.drop(shop,movie);
- * List<List<Integer>> param_4 = obj.report();
- */
