ba14001a3123d2ba8c7c17867e6d93da539f35d2
Rk Roy <rajr86642@gmail.com>
2025-10-03 09:37:18 +0530

    Time: 20 ms (51.73%), Space: 45.8 MB (97.65%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -1,59 +1,57 @@
 class Solution {
-    static class Pair {
-        int value, row, col;
-        public Pair(int value, int row, int col) {
-            this.value = value;
+    static class Tuple {
+        int row, col, cost;
+        public Tuple(int row, int col, int cost) {
             this.row = row;
             this.col = col;
+            this.cost = cost;
         }
         @Override
         public String toString() {
-            return "(" + value + " " + row + " " + col + ")";
+            return "(" + row + " " + col + " " + cost + ")";
         }
     }
-    static class custom_sort implements Comparator<Pair> {
+
+    static class customSort implements Comparator<Tuple> {
         @Override
-        public int compare(Pair first, Pair second) {
-            return Integer.compare(first.value, second.value);
+        public int compare(Tuple first, Tuple second) {
+            return Integer.compare(first.cost, second.cost);
         }
     }
+    
     public int trapRainWater(int[][] arr) {
         int n = arr.length, m = arr[0].length;
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new custom_sort());
         int vis[][] = new int[n][m];
-        for (int j = 0; j < m; j++) {
-            vis[0][j] = 1;
-            pq.offer(new Pair(arr[0][j], 0, j));
-        }
-        for (int i = 0; i < n; i++) {
-            vis[i][m - 1] = 1;
-            pq.offer(new Pair(arr[i][m - 1], i, m - 1));
-        }
-        for (int i = 0; i < n; i++) {
-            vis[i][0] = 1;
-            pq.offer(new Pair(arr[i][0], i, 0));
-        }
-        for (int j = 0; j < m; j++) {
-            vis[n - 1][j] = 1;
-            pq.offer(new Pair(arr[n - 1][j], n - 1, j));
-        }
+        PriorityQueue<Tuple> pq = new PriorityQueue<>(new customSort());
         int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int i = 0; i < n; i++) {
+            pq.offer(new Tuple(i, 0, arr[i][0]));
+            pq.offer(new Tuple(i, m - 1, arr[i][m - 1]));
+            vis[i][0] = 1;
+            vis[i][m - 1] = 1;
+        }      
+
+        for (int j = 0; j < m; j++) {
+            pq.offer(new Tuple(0, j, arr[0][j]));
+            pq.offer(new Tuple(n - 1, j, arr[n - 1][j]));
+            vis[0][j] = 1;
+            vis[n - 1][j] = 1;
+        }   
+
         int res = 0;
         while (pq.size() > 0) {
-            int cr = pq.peek().row;
-            int cc = pq.peek().col;
-            int cval = pq.peek().value;
+            int currRow = pq.peek().row, currCol = pq.peek().col, currCost = pq.peek().cost;
             pq.poll();
             for (int dire[] : dir) {
-                int nr = cr + dire[0];
-                int nc = cc + dire[1];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m && vis[nr][nc] == 0) {
-                    vis[nr][nc] = 1;
-                    res += Math.max(0, cval - arr[nr][nc]);
-                    pq.offer(new Pair(Math.max(cval, arr[nr][nc]), nr, nc));
+                int newRow = currRow + dire[0], newCol = currCol + dire[1];
+                if (newRow < n && newRow >= 0 && newCol < m && newCol >= 0 && vis[newRow][newCol] == 0) {
+                    vis[newRow][newCol] = 1;
+                    res += Math.max(0, currCost - arr[newRow][newCol]);
+                    pq.offer(new Tuple(newRow, newCol, Math.max(arr[newRow][newCol], currCost)));
                 }
             }
         }
-        return res;
+        return res; 
     }
 }
