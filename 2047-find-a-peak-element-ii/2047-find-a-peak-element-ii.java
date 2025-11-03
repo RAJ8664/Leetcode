9c35a7990a486720b93395c8d5ee7da7349efa73
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-11-03 12:53:31 +0545

    Time: 0 ms (100%), Space: 115.6 MB (5.96%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int n = mat.length, m = mat[0].length;
-        int low = 0, high = m - 1;
+        int low = 0, high = m - 1, ans = -1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            int maxR = -1, maxEle = -1;
+            int maxEle = 0, maxR = -1;
             for (int i = 0; i < n; i++) {
                 if (mat[i][mid] > maxEle) {
                     maxEle = mat[i][mid];
                     maxR = i;
-                }
-            }   
-
+                } 
+            }
             int left = -1, right = -1;
-            if (mid - 1 >= 0) 
+            if (mid - 1 >= 0)
                 left = mat[maxR][mid - 1];
             if (mid + 1 < m) 
-                right = mat[maxR][mid + 1];
-            if (mat[maxR][mid] > left && mat[maxR][mid] > right) 
+                right = mat[maxR][mid + 1]; 
+            if (maxEle > left && maxEle > right)
                 return new int[]{maxR, mid};
-            else {
-                if (right > mat[maxR][mid])
-                    low = mid + 1;
-                else 
-                    high = mid - 1;
-            } 
+            if (left > maxEle)
+                high = mid - 1;
+            else 
+                low = mid + 1;
         }
         return new int[]{-1, -1};
     }
