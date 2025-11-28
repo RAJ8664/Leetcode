5993ff854f4b01a901d538e2bd958cf53a55b034
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-11-28 09:43:31 +0530

    Time: 25 ms (78.49%), Space: 97.3 MB (10.75%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.java
@@ -3,20 +3,22 @@ class Solution {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0; i <= n + 1; i++) adj.add(new ArrayList<>());
         for(int current[] : edges) {
-            int u = current[0];
-            int v = current[1];
+            int u = current[0], v = current[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
         if(n == 1) {
-            if(values[0] % k == 0) return 1;
-            else return 0;
+            if  (values[0] % k == 0) 
+                return 1;
+            else 
+                return 0;
         }
         int count = 0;
         long res[] = new long[n + 1];
         dfs(1,-1, adj, values,res);
         for(int i = 0; i < n; i++) {
-            if(res[i] % k == 0) count++;
+            if(res[i] % k == 0) 
+                count++;
         }
         return count;
     }
@@ -26,10 +28,12 @@ class Solution {
             return;
         }
         for(int v : adj.get(u)) {
-            if(v != par) dfs(v, u , adj, values,res);
+            if(v != par) 
+                dfs(v, u , adj, values,res);
         }
         for(int v : adj.get(u)) {
-            if(v != par) res[u] += res[v];
+            if(v != par) 
+                res[u] += res[v];
         }
         res[u] += values[u];
     }
