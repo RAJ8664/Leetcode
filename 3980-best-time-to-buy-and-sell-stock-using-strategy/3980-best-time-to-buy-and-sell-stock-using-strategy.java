d383d11e238509128b93de05a2a32fd0902260b9
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-12-18 11:28:30 +0530

    Time: 9 ms (7.89%), Space: 102.4 MB (35.53%) - LeetHub

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.java
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/3980-best-time-to-buy-and-sell-stock-using-strategy.java
@@ -5,24 +5,20 @@ class Solution {
         int n = prices.length;
         pref = new long[n];
         pricePref = new long[n];
-      
         pref[0] = strategy[0] * 1L * prices[0];
         pricePref[0] = prices[0] * 1L;
         for (int i = 1; i < n; i++) { 
             pref[i] = pref[i - 1] + (prices[i] * 1L * strategy[i]);
             pricePref[i] = pricePref[i - 1] + prices[i] * 1L;
         } 
-       
         long ans = 0;
         for (int i = 0; i < n; i++) 
             ans += prices[i] * 1L * strategy[i];
-
         for (int i = 0; i < n; i++) {
             long currentSum = 0;
             if (i + k - 1 < n) {
-                if (i - 1 >= 0) {
+                if (i - 1 >= 0) 
                     currentSum += pref[i - 1];
-                }
                 if (i + k < n) {
                     currentSum += pref[n - 1];
                     currentSum -= pref[i + k - 1];
