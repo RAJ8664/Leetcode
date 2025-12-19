96e3b6e9ec947f39bc720808196a7e909199676c
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-12-19 09:09:46 +0530

    Time: 59 ms (61.39%), Space: 193.5 MB (25.95%) - LeetHub

--- 2213-find-all-people-with-secret/2213-find-all-people-with-secret.java
+++ 2213-find-all-people-with-secret/2213-find-all-people-with-secret.java
@@ -1,68 +1,42 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.PriorityQueue;
-
 class Solution {
     static class Pair {
         int node, time;
-        public Pair(int node, int time) {
-            this.node = node;
-            this.time = time;
-        }
-        @Override
-        public String toString() {
-            return "Pair{" +
-                   "node=" + node +
-                   ", time=" + time +
-                   '}';
+        Pair(int n, int t) {
+            node = n;
+            time = t;
         }
     }
-    static class customSort implements Comparator<Pair> {
-        @Override
-        public int compare(Pair first, Pair second) {
-            return Integer.compare(first.time, second.time);
-        }
-    }
-    private ArrayList<ArrayList<Pair >> adj;
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        List<Integer> ans = new ArrayList<>();
-        adj = new ArrayList<>();
-        for (int i = 0; i <= n + 1; i++)
-            adj.add(new ArrayList<>());
-
-        for (int edge[] : meetings) {
-            int u = edge[0], v = edge[1], t = edge[2];
-            adj.get(u).add(new Pair(v, t));
-            adj.get(v).add(new Pair(u, t));
+        List<List<Pair>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] m : meetings) {
+            adj.get(m[0]).add(new Pair(m[1], m[2]));
+            adj.get(m[1]).add(new Pair(m[0], m[2]));
         }
-
-        int vis[] = new int[n + 1];
-        PriorityQueue<Pair> pq = new PriorityQueue<>(new customSort());
-        pq.offer(new Pair(firstPerson, 0));
+        int[] timeKnown = new int[n];
+        Arrays.fill(timeKnown, Integer.MAX_VALUE);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.time - b.time);
+        timeKnown[0] = 0;
+        timeKnown[firstPerson] = 0;
         pq.offer(new Pair(0, 0));
-        vis[0] = 1;
-        vis[firstPerson] = 1;
-        HashSet<Integer> set = new HashSet<>();
-        while (pq.size() > 0) {
-            int currNode = pq.peek().node;
-            int currTime = pq.peek().time;
-            set.add(currNode);
-            vis[currNode] = 1;
-            pq.poll();
-            for (int i = 0; i < adj.get(currNode).size(); i++) {
-                int childNode = adj.get(currNode).get(i).node;
-                int childTime = adj.get(currNode).get(i).time;
-                if (vis[childNode] == 1)
-                    continue;
-                if (childTime >= currTime)
-                    pq.offer(new Pair(childNode, childTime));
+        pq.offer(new Pair(firstPerson, 0));
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
+            if (cur.time > timeKnown[cur.node]) 
+                continue;
+            for (Pair next : adj.get(cur.node)) {
+                if (next.time >= cur.time &&
+                    next.time < timeKnown[next.node]) {
+                    timeKnown[next.node] = next.time;
+                    pq.offer(new Pair(next.node, next.time));
+                }
             }
         }
-
-        for (int ele : set)
-            ans.add(ele);
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (timeKnown[i] != Integer.MAX_VALUE)
+                ans.add(i);
+        }
         return ans;
     }
 }
