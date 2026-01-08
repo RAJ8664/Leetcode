def32c21f796846e8ff01cf27b6bdcf98333eaa0
Rk Roy🇳🇵 <rajr86642@gmail.com>
2026-01-08 11:55:00 +0530

    Time: 9 ms (85.19%), Space: 46.5 MB (42.96%) - LeetHub

--- 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/1569-max-dot-product-of-two-subsequences.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int fun(int[] arr1,int[] arr2,int n,int m,int[][] dp,int i,int j){
-        if(i==n || j==m)
+    private int dp[][];
+    public int solve(int arr[], int brr[], int i, int j) {
+        if (i == arr.length || j == brr.length)
             return 0;
-        if(dp[i][j]!=-1)
+        if (dp[i][j] != -1) 
             return dp[i][j];
-        int skipI = fun(arr1,arr2,n,m,dp,i+1,j); 
-        int skipJ = fun(arr1,arr2,n,m,dp,i,j+1);
-        int pickIJ = (arr1[i]*arr2[j]) + fun(arr1,arr2,n,m,dp,i+1,j+1);
-        return dp[i][j] = Math.max(pickIJ,Math.max(skipI,skipJ));
+        int op1 = solve(arr, brr, i + 1, j);
+        int op2 = solve(arr, brr, i, j + 1);
+        int op3 = arr[i] * brr[j] + solve(arr, brr, i + 1, j + 1);
+        return dp[i][j] = Math.max(op1, Math.max(op2, op3));
     }
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int[][] dp = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++)
-                dp[i][j] = -1;
+        int n = nums1.length, m = nums2.length;
+        dp = new int[n + 1][m + 1];
+        for (int current[] : dp)
+            Arrays.fill(current, -1);
+        int res = Integer.MIN_VALUE;
+        res = solve(nums1, nums2, 0, 0);
+        if (res == 0) {
+            int maxi = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) 
+                    maxi = Math.max(maxi, nums1[i] * nums2[j]);
+            }
+            return maxi;
         }
-        int ans = Integer.MIN_VALUE;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++)
-                ans = Math.max(ans,nums1[i]*nums2[j]);
-        }
-        int answer = fun(nums1,nums2,n,m,dp,0,0);
-        if(answer==0)
-            return ans;
-        return answer;
+        return res;
     }
 }
