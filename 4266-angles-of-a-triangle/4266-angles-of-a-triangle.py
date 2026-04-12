69978adcfa5348d7ed7682d37da406f00d184a88
Rk Roy🇳🇵 <rajr86642@gmail.com>
2026-04-12 12:08:23 +0530

    4266-ANGLES-OF-A-TRIANGLE Stats: Time: 0 ms (100%), Space: 19.5 MB (85.51%) - LeetHub

---
+++ 4266-angles-of-a-triangle/4266-angles-of-a-triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def triangle_angles(self, a, b, c) -> list[float]:
+        A = math.acos((b*b + c*c - a*a) / (2*b*c))
+        B = math.acos((a*a + c*c - b*b) / (2*a*c))
+        C = math.acos((a*a + b*b - c*c) / (2*a*b))
+        
+        A = math.degrees(A)
+        B = math.degrees(B)
+        C = math.degrees(C)
+        
+        return [round(A, 5), round(B, 5), round(C, 5)]
+
+    def internalAngles(self, sides: list[int]) -> list[float]:
+        res: list[float] = []
+        if (sides[0] + sides[1] <= sides[2]) or (sides[1] + sides[2] <= sides[0]) or (sides[2] + sides[0] <= sides[1]):
+            return res
+        res = self.triangle_angles(sides[0], sides[1], sides[2])
+        res.sort()
+        return res 
