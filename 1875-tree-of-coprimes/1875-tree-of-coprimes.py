5cfa395f5f47d1ee1697d996cd50bef01af84223
Rk Roy🇳🇵 <rajr86642@gmail.com>
2026-06-25 21:46:24 +0530

    1875-TREE-OF-COPRIMES Stats: Time: 1955 ms (5.88%), Space: 96.9 MB (45.59%) - LeetHub

--- 1875-tree-of-coprimes/1875-tree-of-coprimes.py
+++ 1875-tree-of-coprimes/1875-tree-of-coprimes.py
@@ -2,7 +2,7 @@
 # Alone with music ............................ 
 
 class Solution:
-    def build_dp(self, u, par, adj, dp, depth) -> None:
+    def build_dp(self, u, par, adj, dp) -> None:
         dp[u][0] = par
         
         for i in range(1, 19):
@@ -10,8 +10,7 @@ class Solution:
 
         for child in adj[u]:
             if child != par:
-                depth[child] = 1 + depth[u]
-                self.build_dp(child, u, adj, dp, depth)
+                self.build_dp(child, u, adj, dp)
 
     def find_kth_parent(self, u, k, dp) -> None:
         count = 0
@@ -24,29 +23,22 @@ class Solution:
         
         return u
 
-    def dfs(self, u, par, adj, dp, depth, pos, nums, level, ans):
+    def dfs(self, u, par, adj, dp, pos, nums, level, ans):
         pos[nums[u]].append(level)
 
         for child in adj[u]:
             if child != par:
-                curr_val = nums[child]
-                flag = False
-                mini_pos = 10**9
+                best_pos = 10**9
                 for i in range(1, 51):
-                    if math.gcd(curr_val, i) == 1:
-                        positions = pos[i]
-                        if len(positions) == 0: continue
-                        flag = True
-                        best_pos = (level + 1) - positions[len(positions) - 1]          
-                        mini_pos = min(mini_pos, best_pos)
+                    if math.gcd(nums[child], i) == 1:
+                        if len(pos[i]) == 0: 
+                            continue
+                        best_pos = min(best_pos, (level + 1) - pos[i][len(pos[i]) - 1])          
 
-                if flag:
-                    res = self.find_kth_parent(child, mini_pos, dp)
-                    ans[child] = res
-                else:
-                    ans[child] = -1
+                if best_pos != 10**9:
+                    ans[child] = self.find_kth_parent(child, best_pos, dp)
 
-                self.dfs(child, u, adj, dp, depth, pos, nums, level + 1, ans)
+                self.dfs(child, u, adj, dp, pos, nums, level + 1, ans)
                     
         pos[nums[u]].pop(len(pos[nums[u]]) - 1)
 
@@ -55,7 +47,6 @@ class Solution:
         n = len(nums)
         adj = [[] for _ in range(n + 1)]
         dp = [[0] * 19 for _ in range(n + 1)]
-        depth = [0] * (n + 1)
         pos = [[] for _ in range(51)]
         ans = [-1] * n
 
@@ -64,9 +55,9 @@ class Solution:
             adj[u].append(v)
             adj[v].append(u)
 
-        self.build_dp(1, -1, adj, dp, depth)
+        self.build_dp(1, -1, adj, dp)
 
-        self.dfs(1, -1, adj, dp, depth, pos, nums, 0, ans)
+        self.dfs(1, -1, adj, dp, pos, nums, 0, ans)
 
         ans.pop(0)
         for i in range(len(ans)):
