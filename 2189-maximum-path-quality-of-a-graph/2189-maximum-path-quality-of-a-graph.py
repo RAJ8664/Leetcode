2f4a45b70ea9cc89bb3e1dc9def43cad669c48d3
Rk Roy🇳🇵 <rajr86642@gmail.com>
2026-06-26 20:03:03 +0530

    Time: 729 ms (73.14%), Space: 21.9 MB (57.24%) - LeetHub

---
+++ 2189-maximum-path-quality-of-a-graph/2189-maximum-path-quality-of-a-graph.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximalPathQuality(self, vals, edges, maxTime):
+        n = len(vals)
+        adj = [[] for _ in range(n)]
+        for u, v, t in edges:
+            adj[u].append((v, t))
+            adj[v].append((u, t))
+
+        vis = [0] * n
+        self.max_val = 0
+
+        def dfs(i, time, val):
+            vis[i] += 1
+            if vis[i] == 1:
+                val += vals[i]
+
+            if i == 0:
+                self.max_val = max(self.max_val, val)
+
+            for j, t in adj[i]:
+                if time - t >= 0:
+                    dfs(j, time - t, val)
+
+            vis[i] -= 1
+
+        dfs(0, maxTime, 0)
+        return self.max_val
