45df0d489ab5a2c1c0e95ef92648177490569ff4
Rk Roy🇳🇵 <rajr86642@gmail.com>
2026-06-28 19:14:50 +0530

    3724-FILTER-OCCUPIED-INTERVALS Stats: Time: 276 ms (6.2%), Space: 41.2 MB (25.6%) - LeetHub

--- 3724-filter-occupied-intervals/3724-filter-occupied-intervals.py
+++ 3724-filter-occupied-intervals/3724-filter-occupied-intervals.py
@@ -1,49 +1,30 @@
-class Pair:
-    def __init__(self, start: int, end: int) -> None:
-        self.start = start
-        self.end = end
-
-    def __repr__(self) -> str:
-        return f"({self.start}, {self.end})"
-
-    def __lt__(self, other):
-        return self.start <= other.start
-        
 class Solution:
     def filterOccupiedIntervals(self, occupiedIntervals: List[List[int]], freeStart: int, freeEnd: int) -> List[List[int]]:
         n = len(occupiedIntervals)
-        arr = []
         res = []
 
-        for i in range(n):
-            arr.append(Pair(occupiedIntervals[i][0], occupiedIntervals[i][1]))
-
-        arr.sort()
-
-        print(arr)
+        occupiedIntervals.sort(key = lambda x: (x[0], x[1])) 
 
         left, right = 0, 0
         while left < n:
             right = left
-            curr_maxi = arr[left].end
-            while right < n and arr[right].start <= curr_maxi + 1:
-                curr_maxi = max(curr_maxi, arr[right].end)
+            curr_maxi = occupiedIntervals[left][1]
+            while right < n and occupiedIntervals[right][0] <= curr_maxi + 1:
+                curr_maxi = max(curr_maxi, occupiedIntervals[right][1])
                 right += 1
-            res.append((arr[left].start, curr_maxi))
+            res.append((occupiedIntervals[left][0], curr_maxi))
             if left == right:
                 left += 1
             else: left = right
 
-        print(res) 
-        
         ans = []
         for curr_start, curr_end in res:
-            if curr_start > freeEnd or curr_end < freeStart:
-                ans.append([curr_start, curr_end])
-            
-            elif curr_start >= freeStart and curr_end <= freeEnd:
+            if curr_start >= freeStart and curr_end <= freeEnd:
                 continue
 
+            elif curr_start > freeEnd or curr_end < freeStart:
+                ans.append([curr_start, curr_end])
+            
             elif freeStart > curr_start and freeEnd < curr_end:
                 ans.append([curr_start, max(curr_start, freeStart - 1)])
                 ans.append([freeEnd + 1, curr_end])
